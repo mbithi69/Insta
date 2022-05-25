09768f00195df3a26e7e236506a63eb1564211a3
mbithi69 <105496762+mbithi69@users.noreply.github.com>
2022-05-25 09:58:48 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+<script>
+    function ourVision() {
+        document.getElementById("vision").style.display=value
+    };
+     function ourMission() {
+        document.getElementById("mission").style.display=value
+    };
+</script>
